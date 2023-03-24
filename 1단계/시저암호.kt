e2ee25806f24ea04e2a5d8bf1866416201e6ffaf
JeongMyeongGyun <wjdaudrbs456@naver.com>
2023-03-25 03:09:17 +0900

    시저암호

--- 1단계/시저암호.kt
+++ 1단계/시저암호.kt
@@ -1,5 +1,6 @@
 package com.example.algoritnmmaster
 
+//시저 암호
 
 //문제 설명
 //어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
@@ -17,20 +18,29 @@ package com.example.algoritnmmaster
 //"a B z"	4	    "e F d"
 
 
-fun main(){
-solution4("AsBc", 1)
+fun main() {
+//    solution4("AsBc", 1)
+
 }
 
+//알게된것들
+//아스크 코드
+//a~z 97~122
+//A~Z 65~90
+
 fun solution4(s: String, n: Int): String {
-    return s.toCharArray().map{
-        when(it){
+    return s.toCharArray().map {
+        println("it :: $it")
+        when (it) {
             in 'A'..'Z' -> {
-                if(it + n > 'Z') it + n - 26 else it + n
-            }in 'a'..'z' -> {
-            if(it + n > 'z') it + n - 26 else it + n
-        }else -> {
-            it
-        }
+                if (it + n > 'Z') it + n - 26 else it + n
+            }
+            in 'a'..'z' -> {
+                if (it + n > 'z') it + n - 26 else it + n
+            }
+            else -> {
+                it
+            }
         }
     }.joinToString("")
 }
@@ -38,8 +48,8 @@ fun solution4(s: String, n: Int): String {
 fun otherSolution(s: String, n: Int): String {
     var answer = ""
 
-    for(c in s) {
-        if(c == ' ') {
+    for (c in s) {
+        if (c == ' ') {
             answer += ' '
         } else {
             answer += getAlphabet(c, n)
@@ -51,12 +61,12 @@ fun otherSolution(s: String, n: Int): String {
 fun getAlphabet(c: Char, n: Int): Char {
     var isUpper = false
     var ascii = c.toInt()
-    if(ascii >= 65 && ascii <= 90) isUpper = true
+    if (ascii >= 65 && ascii <= 90) isUpper = true
     ascii += n
-    if(isUpper) {
-        if(ascii > 90) ascii = ascii - 90 + 65 - 1
+    if (isUpper) {
+        if (ascii > 90) ascii = ascii - 90 + 65 - 1
     } else {
-        if(ascii > 122) ascii = ascii - 122 + 97 - 1
+        if (ascii > 122) ascii = ascii - 122 + 97 - 1
     }
 
     return ascii.toChar()
