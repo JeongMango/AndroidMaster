976faf3b023f04f5cb9fd2c5673e36bb9b255810
jeongmyeonggyun <wjdaudrbs456@naver.com>
2023-06-06 23:21:19 +0900

    [숫자짝궁] 다른방법 풀이

--- 1단계/숫자 짝궁.kt
+++ 1단계/숫자 짝궁.kt
@@ -3,38 +3,59 @@ package com.example.algorizmstudy
 
 import java.util.*
 
+fun main() {
+
+}
 
 //더공부 필요한것들
 //1) .map { it.digitToInt() }.sorted()
 //2) result.sortedByDescending{ it }.forEach {
 //            answer.append(it)
 //        }
-class Solution {
-    fun solution(X: String, Y: String): String {
-        var answer = StringBuilder()
+fun solution(X: String, Y: String): String {
+    var answer = StringBuilder()
 
-        val result = mutableListOf<Int>()
-        val xList = X.toCharArray().map { it.digitToInt() }.sorted()
-        val yList = Y.toCharArray().map { it.digitToInt() }.sorted()
+    val result = mutableListOf<Int>()
+    val xList = X.toCharArray().map { it.digitToInt() }.sorted()
+    val yList = Y.toCharArray().map { it.digitToInt() }.sorted()
 
-        var index = 0
-        xList.forEach {
-            for(i in index until yList.size) {
-                if(yList[i] == it) {
-                    result.add(it)
-                    index = i + 1
-                    break
-                }
+    var index = 0
+    xList.forEach {
+        for (i in index until yList.size) {
+            if (yList[i] == it) {
+                result.add(it)
+                index = i + 1
+                break
             }
         }
-
-        result.sortedByDescending{ it }.forEach {
-            answer.append(it)
-        }
-
-        if(answer.isEmpty()) return "-1"
-        else if(answer[0] == '0') return "0"
-
-        return answer.toString()
     }
+
+    result.sortedByDescending { it }.forEach {
+        answer.append(it)
+    }
+
+    if (answer.isEmpty()) return "-1"
+    else if (answer[0] == '0') return "0"
+
+    return answer.toString()
 }
+
+fun otherSolution(X: String, Y: String): String {
+    var sameNums: String = ""
+    var yNums = Y
+
+    for (i in 0..9) {
+        val intToChar = (i + 48).toChar()
+        val xContainCount = X.filter { it == intToChar }.count()
+        val yContainCount = Y.filter { it == intToChar }.count()
+        val minCount = if (xContainCount >= yContainCount) yContainCount else xContainCount
+        if (minCount > 0) sameNums += intToChar.toString().repeat(minCount)
+    }
+
+    if (sameNums.length == 0) return "-1"
+    sameNums = String(sameNums.toCharArray().apply { sortDescending() })
+    if (sameNums[0] == '0') return "0" else return sameNums
+}
+
+
+
